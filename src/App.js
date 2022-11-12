b19a59bb9c25d67474ffe12a6ef0fe347ad5c804
jamest11 <jameswtory@gmail.com>
2022-11-12 16:16:27 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form, { initialFormState } from './Form';
 import './App.css';
 
+
 function App() {
+  
+  const [formData, setFormData] = useState({initialFormState});
+  const [showData, setShowData] = useState(false);
+
+  const formDisplay = {
+    email: 'Email',
+    name: 'Full Name',
+    address1: 'Address',
+    city: 'City',
+    province: 'Province',
+    postcode: 'Post Code'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Entry Form</h1>
+      <Form submitForm={(formData) => {
+          setFormData(formData)
+          setShowData(true)
+        }
+      }/>
+      
+      {showData && (
+        <div className="container" style={{ width: "50em" }}>
+          {Object.keys(formDisplay).map((key) => {
+              return (
+                <div className="row m-2" key={key}>
+                  <div className="col text-end pe-1">
+                    <span className="text-success h5">{formDisplay[key]}:</span>
+                  </div>
+                  <div className="col text-start ps-1">
+                    <span className="h5">{formData[key]}</span>
+                  </div>
+                </div>
+              );
+          })}
+        </div>
+      )} 
     </div>
   );
 }
